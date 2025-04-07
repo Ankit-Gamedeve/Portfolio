5f748dd2ee76948a6a5080c9ccd4abceec38c707
Ankit-Gamedeve <tanishk.sharma21@gmail.com>
2025-04-08 00:04:25 +0530

    First uplode

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function sayHello() {
+    alert("hey i have created alert!");
+  }
+  
+  function changeGreeting() {
+    document.getElementById("greet").innerText = "You're awesome 😎";
+  }
+  
+  function changeBackground() {
+    document.body.style.backgroundColor = "#f0f0f0";
+  }
+  function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+  }
+  function Main() {
+    document.body.style.color = "#00bcd4";
+  }
